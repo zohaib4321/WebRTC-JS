585fce4fcbad59b7afe312bedce56d3cf0bacbaa
Zohaib <zohaib4321@gmail.com>
2024-06-07 11:56:25 +0500

    test echo cancellation for bugs

--- script.js
+++ script.js
@@ -28,7 +28,11 @@ let constraints = {
 		width: { min: 640, ideal: 1920, max: 1920 },
 		height: { min: 480, ideal: 1080, max: 1080 },
 	},
-	audio: { echoCancellation: true },
+	audio: {
+		echoCancellation: { exact: true },
+		noiseSuppression: { exact: true },
+		autoGainControl: { exact: true },
+	},
 };
 
 async function init(constraints) {
