9152b2316755d0a932de93252f6351809afb25e8
Zohaib <zohaib4321@gmail.com>
2024-06-07 10:11:43 +0500

    fix echo cancellation

--- script.js
+++ script.js
@@ -31,7 +31,9 @@ let constraints = {
 		width: { min: 640, ideal: 1920, max: 1920 },
 		height: { min: 480, ideal: 1080, max: 1080 },
 	},
-	audio: true,
+	audio: {
+		echoCancellation: true
+	},
 };
 
 async function init(constraints) {
