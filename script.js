10e4b40a529530088efcb8768c6560a0eccf16d3
Zohaib <zohaib4321@gmail.com>
2024-06-07 11:44:13 +0500

    fix echo cancellation typeO

--- script.js
+++ script.js
@@ -18,10 +18,7 @@ let peerConnection;
 const servers = {
 	iceServers: [
 		{
-			urls: [
-				"stun:stun1.l.google.com:19302", 
-				"stun:stun2.l.google.com:19302"
-			],
+			urls: ["stun:stun1.l.google.com:19302", "stun:stun2.l.google.com:19302"],
 		},
 	],
 };
@@ -31,9 +28,7 @@ let constraints = {
 		width: { min: 640, ideal: 1920, max: 1920 },
 		height: { min: 480, ideal: 1080, max: 1080 },
 	},
-	audio: {
-		echoCancellation: true
-	},
+	audio: { echoCancellation: true },
 };
 
 async function init(constraints) {
